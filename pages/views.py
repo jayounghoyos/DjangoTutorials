f8384582ba602ba24e75ad8849ea702fc9284dd0
jayounghoyos <jayoungh@eafit.edu.co>
2025-07-26 11:28:16 -0500

    App running

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse # new
+from django.views.generic import TemplateView
 
 # Create your views here.
-def homePageView(request): # new
-    return HttpResponse('Hello World!') # new
+# def homePageView(request): # new
+#     return HttpResponse('Hello World!') # new
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
