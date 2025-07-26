307f173391ffe34d1ddb46619ac03d5bb1ab865a
jayounghoyos <jayoungh@eafit.edu.co>
2025-07-26 11:42:38 -0500

    About us

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,15 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by:  Juan Andrés Young Hoyos",
+        })
+        return context
